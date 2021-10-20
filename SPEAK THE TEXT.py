f06a7b68396385f7814bf1cce186f7e5eeb81c3f
RishiRishiRishiRishi <92801984+RishiRishiRishiRishi@users.noreply.github.com>
2021-10-20 14:47:28 +0530

    Add files via upload

---
+++ SPEAK THE TEXT.py
@@ -0,0 +1,19 @@
+import pyttsx3
+from tkinter import *
+root = Tk()
+root.title("SPEAK THE TEXT")
+root.geometry("800x500")
+def talk():
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 200)
+    engine.say(my_entry.get())
+    engine.runAndWait()
+    my_entry.delete(0, END)
+
+
+my_entry = Entry(root, font=("Helvetica", 28))
+my_entry.pack(pady=20)
+my_button = Button(root, text="Speak", command=talk)
+my_button.pack(pady=20)
+
+root.mainloop()
